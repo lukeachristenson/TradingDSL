324246d138bd1b587a0b405beffae7c5860e734d
Smaran-Teja-P <smaranteja32@gmail.com>
2025-03-09 20:11:36 -0400

    code reorganization, 1y50 data

--- date/python retrieval scripts/hyol_data.py
+++ date/python retrieval scripts/hyol_data.py
@@ -87,17 +87,67 @@ def is_weekend_or_holiday(date_str: str):
         return True # if exception, assume that aapl doesn't have data for some reason, so market holiday
 
 
-start_date = "2020-01-01"
-end_date = "2024-12-31"
+START_DATE = "2024-01-01"
+END_DATE = "2024-12-31"
 bucket = []
 
-cur_date = start_date
+cur_date = START_DATE
 data = [["Date", "Ticker", "Open", "Close"]]
 
 
-bucket = "A,AAPL,ABBV,ABNB,ABT,ACGL,ACN,ADBE,ADI,ADM,ADP,ADSK,AEE,AEP,AES,AFL,AIG,AIZ,AJG,AKAM,ALB,ALGN,ALL,ALLE,AMAT,AMCR,AMD,AME,AMGN,AMP,AMT,AMTM,AMZN,ANET,ANSS,AON,AOS,APA,APD,APH,APTV,ARE,ATO,AVB,AVGO,AVY,AWK,AXON,AXP,AZO,BA,BAC,BALL,BAX,BBY,BDX,BEN,BF-B,BG,BIIB,BK,BKNG,BKR,BLDR,BLK,BMY,BR,BRK-B,BRO,BSX,BWA,BX,BXP,C,CAG,CAH,CARR,CAT,CB,CBOE,CBRE,CCI,CCL,CDNS,CDW,CE,CEG,CF,CFG,CHD,CHRW,CHTR,CI,CINF,CL,CLX,CMCSA,CME,CMG,CMI,CMS,CNC,CNP,COF,COO,COP,COR,COST,CPAY,CPB,CPRT,CPT,CRL,CRM,CRWD,CSCO,CSGP,CSX,CTAS,CTLT,CTRA,CTSH,CTVA,CVS,CVX,CZR,D,DAL,DAY,DD,DE,DECK,DELL,DFS,DG,DGX,DHI,DHR,DIS,DLR,DLTR,DOC,DOV,DOW,DPZ,DRI,DTE,DUK,DVA,DVN,DXCM,EA,EBAY,ECL,ED,EFX,EG,EIX,EL,ELV,EMN,EMR,ENPH,EOG,EPAM,EQIX,EQR,EQT,ERIE,ES,ESS,ETN,ETR,EVRG,EW,EXC,EXPD,EXPE,EXR,F,FANG,FAST,FCX,FDS,FDX,FE,FFIV,FI,FICO,FIS,FITB,FMC,FOX,FOXA,FRT,FSLR,FTNT,FTV,GD,GDDY,GE,GEHC,GEN,GEV,GILD,GIS,GL,GLW,GM,GNRC,GOOG,GOOGL,GPC,GPN,GRMN,GS,GWW,HAL,HAS,HBAN,HCA,HD,HES,HIG,HII,HLT,HOLX,HON,HPE,HPQ,HRL,HSIC,HST,HSY,HUBB,HUM,HWM,IBM,ICE,IDXX,IEX,IFF,INCY,INTC,INTU,INVH,IP,IPG,IQV,IR,IRM,ISRG,IT,ITW,IVZ,J,JBHT,JBL,JCI,JKHY,JNJ,JNPR,JPM,K,KDP,KEY,KEYS,KHC,KIM,KKR,KLAC,KMB,KMI,KMX,KO,KR,KVUE,L,LDOS,LEN,LH,LHX,LIN,LKQ,LLY,LMT,LNT,LOW,LRCX,LULU,LUV,LVS,LW,LYB,LYV,MA,MAA,MAR,MAS,MCD,MCHP,MCK,MCO,MDLZ,MDT,MET,META,MGM,MHK,MKC,MKTX,MLM,MMC,MMM,MNST,MO,MOH,MOS,MPC,MPWR,MRK,MRNA,MS,MSCI,MSFT,MSI,MTB,MTCH,MTD,MU,NCLH,NDAQ,NDSN,NEE,NEM,NFLX,NI,NKE,NOC,NOW,NRG,NSC,NTAP,NTRS,NUE,NVDA,NVR,NWS,NWSA,NXPI,O,ODFL,OKE,OMC,ON,ORCL,ORLY,OTIS,OXY,PANW,PARA,PAYC,PAYX,PCAR,PCG,PEG,PEP,PFE,PFG,PG,PGR,PH,PHM,PKG,PLD,PLTR,PM,PNC,PNR,PNW,PODD,POOL,PPG,PPL,PRU,PSA,PSX,PTC,PWR,PYPL,QCOM,QRVO,RCL,REG,REGN,RF,RJF,RL,RMD,ROK,ROL,ROP,ROST,RSG,RTX,RVTY,SBAC,SBUX,SCHW,SHW,SJM,SLB,SMCI,SNA,SNPS,SO,SOLV,SPG,SPGI,SRE,STE,STLD,STT,STX,STZ,SW,SWK,SWKS,SYF,SYK,SYY,T,TAP,TDG,TDY,TECH,TEL,TER,TFC,TFX,TGT,TJX,TMO,TMUS,TPL,TPR,TRGP,TRMB,TROW,TRV,TSCO,TSLA,TSN,TT,TTWO,TXN,TXT,TYL,UAL,UBER,UDR,UHS,ULTA,UNH,UNP,UPS,URI,USB,V,VICI,VLO,VLTO,VMC,VRSK,VRSN,VRTX,VST,VTR,VTRS,VZ,WAB,WAT,WBA,WBD,WDC,WEC,WELL,WFC,WM,WMB,WMT,WRB,WST,WTW,WY,WYNN,XEL,XOM,XYL,YUM,ZBH,ZBRA,ZTS".split(",")
+bucket = [
+    'AAPL',  # Apple Inc.
+    'MSFT',  # Microsoft Corporation
+    'NVDA',  # NVIDIA Corporation
+    'GOOGL', # Alphabet Inc. (Class A)
+    'AMZN',  # Amazon.com, Inc.
+    'META',  # Meta Platforms, Inc.
+    'TSLA',  # Tesla, Inc.
+    'BRK-B', # Berkshire Hathaway Inc. (Class B)
+    'V',     # Visa Inc.
+    'JNJ',   # Johnson & Johnson
+    'XOM',   # Exxon Mobil Corporation
+    'WMT',   # Walmart Inc.
+    'JPM',   # JPMorgan Chase & Co.
+    'MA',    # Mastercard Incorporated
+    'PG',    # Procter & Gamble Company
+    'HD',    # The Home Depot, Inc.
+    'CVX',   # Chevron Corporation
+    'LLY',   # Eli Lilly and Company
+    'MRK',   # Merck & Co., Inc.
+    'ABBV',  # AbbVie Inc.
+    'PEP',   # PepsiCo, Inc.
+    'KO',    # The Coca-Cola Company
+    'BAC',   # Bank of America Corporation
+    'PFE',   # Pfizer Inc.
+    'AVGO',  # Broadcom Inc.
+    'COST',  # Costco Wholesale Corporation
+    'CSCO',  # Cisco Systems, Inc.
+    'ORCL',  # Oracle Corporation
+    'MCD',   # McDonald's Corporation
+    'CRM',   # Salesforce, Inc.
+    'ACN',   # Accenture plc
+    'DHR',   # Danaher Corporation
+    'TXN',   # Texas Instruments Incorporated
+    'LIN',   # Linde plc
+    'NEE',   # NextEra Energy, Inc.
+    'ABT',   # Abbott Laboratories
+    'DIS',   # The Walt Disney Company
+    'TMUS',  # T-Mobile US, Inc.
+    'AMD',   # Advanced Micro Devices, Inc.
+    'NFLX',  # Netflix, Inc.
+    'NKE',   # NIKE, Inc.
+    'UPS',   # United Parcel Service, Inc.
+    'PM',    # Philip Morris International Inc.
+    'UNH',   # UnitedHealth Group Incorporated
+    'LOW',   # Lowe's Companies, Inc.
+    'INTC',  # Intel Corporation
+    'HON',   # Honeywell International Inc.
+    'RTX',   # RTX Corporation
+    'AMGN'   # Amgen Inc.
+]
 
-while is_date_before(cur_date, end_date):
+while is_date_before(cur_date, END_DATE):
     cur_date = add_one_day(cur_date)
     if (is_weekend_or_holiday(cur_date)):
         continue
@@ -111,7 +161,3 @@ while is_date_before(cur_date, end_date):
 with open('output.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(data)
-
-
-
-save_get_price_cache()
