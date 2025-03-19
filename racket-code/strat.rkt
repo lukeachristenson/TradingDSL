2f817658a31588c8170e3042bfd28fc0d4212a72
Smaran-Teja-P <smaranteja32@gmail.com>
2025-03-18 20:41:04 -0400

    useful date methods + mostly complete top-performers

--- racket-code/strat.rkt
+++ racket-code/strat.rkt
@@ -7,10 +7,56 @@
 (require "data-new.rkt")
 
 ; A Strat is a (-> Date (Hash Ticker Weight))
+; A period is one of 1y, 6m, 1m, 2w
 
-(define (top-performer #:default (period "1y"))
-  period)
+(define stock-tickers
+  '("A" "AAPL" "ABBV" "ABNB" "ABT" "ACGL" "ACN" "ADBE" "ADI" "ADM" "ADP" "ADSK" "AEE" "AEP" "AES" "AFL" "AIG" "AIZ"
+    "AJG" "AKAM" "ALB" "ALGN" "ALL" "ALLE" "AMAT" "AMCR" "AMD" "AME" "AMGN" "AMP" "AMT" "AMTM" "AMZN" "ANET" "ANSS"
+    "AON" "AOS" "APA" "APD" "APH" "APTV" "ARE" "ATO" "AVB" "AVGO" "AVY" "AWK" "AXON" "AXP" "AZO" "BA" "BAC" "BALL"
+    "BAX" "BBY" "BDX" "BEN" "BF-B" "BG" "BIIB" "BK" "BKNG" "BKR" "BLDR" "BLK" "BMY" "BR" "BRK-B" "BRO" "BSX" "BWA"
+    "BX" "BXP" "C" "CAG" "CAH" "CARR" "CAT" "CB" "CBOE" "CBRE" "CCI" "CCL" "CDNS" "CDW" "CE" "CEG" "CF" "CFG" "CHD"
+    "CHRW" "CHTR" "CI" "CINF" "CL" "CLX" "CMCSA" "CME" "CMG" "CMI" "CMS" "CNC" "CNP" "COF" "COO" "COP" "COR" "COST"
+    "CPAY" "CPB" "CPRT" "CPT" "CRL" "CRM" "CRWD" "CSCO" "CSGP" "CSX" "CTAS" "CTLT" "CTRA" "CTSH" "CTVA" "CVS" "CVX"
+    "CZR" "D" "DAL" "DAY" "DD" "DE" "DECK" "DELL" "DFS" "DG" "DGX" "DHI" "DHR" "DIS" "DLR" "DLTR" "DOC" "DOV" "DOW"
+    "DPZ" "DRI" "DTE" "DUK" "DVA" "DVN" "DXCM" "EA" "EBAY" "ECL" "ED" "EFX" "EG" "EIX" "EL" "ELV" "EMN" "EMR" "ENPH"
+    "EOG" "EPAM" "EQIX" "EQR" "EQT" "ERIE" "ES" "ESS" "ETN" "ETR" "EVRG" "EW" "EXC" "EXPD" "EXPE" "EXR" "F" "FANG"
+    "FAST" "FCX" "FDS" "FDX" "FE" "FFIV" "FI" "FICO" "FIS" "FITB" "FMC" "FOX" "FOXA" "FRT" "FSLR" "FTNT" "FTV" "GD"
+    "GDDY" "GE" "GEHC" "GEN" "GEV" "GILD" "GIS" "GL" "GLW" "GM" "GNRC" "GOOG" "GOOGL" "GPC" "GPN" "GRMN" "GS" "GWW"
+    "HAL" "HAS" "HBAN" "HCA" "HD" "HES" "HIG" "HII" "HLT" "HOLX" "HON" "HPE" "HPQ" "HRL" "HSIC" "HST" "HSY" "HUBB"
+    "HUM" "HWM" "IBM" "ICE" "IDXX" "IEX" "IFF" "INCY" "INTC" "INTU" "INVH" "IP" "IPG" "IQV" "IR" "IRM" "ISRG" "IT"
+    "ITW" "IVZ" "J" "JBHT" "JBL" "JCI" "JKHY" "JNJ" "JNPR" "JPM" "K" "KDP" "KEY" "KEYS" "KHC" "KIM" "KKR" "KLAC"
+    "KMB" "KMI" "KMX" "KO" "KR" "KVUE" "L" "LDOS" "LEN" "LH" "LHX" "LIN" "LKQ" "LLY" "LMT" "LNT" "LOW" "LRCX" "LULU"
+    "LUV" "LVS" "LW" "LYB" "LYV" "MA" "MAA" "MAR" "MAS" "MCD" "MCHP" "MCK" "MCO" "MDLZ" "MDT" "MET" "META" "MGM"
+    "MHK" "MKC" "MKTX" "MLM" "MMC" "MMM" "MNST" "MO" "MOH" "MOS" "MPC" "MPWR" "MRK" "MRNA" "MS" "MSCI" "MSFT" "MSI"
+    "MTB" "MTCH" "MTD" "MU" "NCLH" "NDAQ" "NDSN" "NEE" "NEM" "NFLX" "NI" "NKE" "NOC" "NOW" "NRG" "NSC" "NTAP" "NTRS"
+    "NUE" "NVDA" "NVR" "NWS" "NWSA" "NXPI" "O" "ODFL" "OKE" "OMC" "ON" "ORCL" "ORLY" "OTIS" "OXY" "PANW" "PARA" "PAYC"
+    "PAYX" "PCAR" "PCG" "PEG" "PEP" "PFE" "PFG" "PG" "PGR" "PH" "PHM" "PKG" "PLD" "PLTR" "PM" "PNC" "PNR" "PNW"
+    "PODD" "POOL" "PPG" "PPL" "PRU" "PSA" "PSX" "PTC" "PWR" "PYPL" "QCOM" "QRVO" "RCL" "REG" "REGN" "RF" "RJF" "RL"
+    "RMD" "ROK" "ROL" "ROP" "ROST" "RSG" "RTX" "RVTY" "SBAC" "SBUX" "SCHW" "SHW" "SJM" "SLB" "SMCI" "SNA" "SNPS" "SO"
+    "SOLV" "SPG" "SPGI" "SRE" "STE" "STLD" "STT" "STX" "STZ" "SW" "SWK" "SWKS" "SYF" "SYK" "SYY" "T" "TAP" "TDG"
+    "TDY" "TECH" "TEL" "TER" "TFC" "TFX" "TGT" "TJX" "TMO" "TMUS" "TPL" "TPR" "TRGP" "TRMB" "TROW" "TRV" "TSCO"
+    "TSLA" "TSN" "TT" "TTWO" "TXN" "TXT" "TYL" "UAL" "UBER" "UDR" "UHS" "ULTA" "UNH" "UNP" "UPS" "URI" "USB" "V"
+    "VICI" "VLO" "VLTO" "VMC" "VRSK" "VRSN" "VRTX" "VST" "VTR" "VTRS" "VZ" "WAB" "WAT" "WBA" "WBD" "WDC" "WEC"
+    "WELL" "WFC" "WM" "WMB" "WMT" "WRB" "WST" "WTW" "WY" "WYNN" "XEL" "XOM" "XYL" "YUM" "ZBH" "ZBRA" "ZTS"))
 
+(define (top-performer #:period period)
+  (lambda (date)
+    (sort (for/list ([ticker stock-tickers])
+            (list
+             ticker 
+             (/ (stock-data-close
+                 (get-stock-data ticker date)) 
+                (stock-data-close
+                 (get-stock-data ticker (sub-days date period))))
+             ))
+          (lambda (x y) (> (second x)
+                           (second y))))))
 
-(top-performer )
+(define 1y 364)
+(define 6m 182)
+(define 1m 30)
+(define 2w 14)
+(define 1w 7)
+(define 1d 1)
 
+((top-performer #:period 1y) (reduced-date 2024 12 31))
